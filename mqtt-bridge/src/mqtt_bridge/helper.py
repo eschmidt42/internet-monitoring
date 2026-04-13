463f0d7365acbede780c0bfd8920b7c1248bdfee
eschmidt42 <11818904+eschmidt42@users.noreply.github.com>
2026-04-13 09:40:02 +0200

    chore: migrated pieces to helper.py and adjusted tests

--- mqtt-bridge/src/mqtt_bridge/helper.py
+++ mqtt-bridge/src/mqtt_bridge/helper.py
@@ -1,24 +1,38 @@
 import logging
+from typing import TypedDict
 
 import paho.mqtt.publish as paho_publish
 
 logger = logging.getLogger(__name__)
 
 
-def mqtt_auth(*, user, password):
+class AuthParameter(TypedDict, total=False):
+    username: str
+    password: str | None
+
+
+def mqtt_auth(*, user: str | None, password: str | None) -> AuthParameter | None:
     if user and password:
         return {"username": user, "password": password}
     return None
 
 
-def publish(payload: str, *, user, password, topic, host, port):
+def publish(
+    payload: str,
+    *,
+    user: str | None,
+    password: str | None,
+    topic: str,
+    host: str,
+    port: int,
+) -> None:
     auth = mqtt_auth(user=user, password=password)
     paho_publish.single(
         topic=topic,
         payload=payload,
         hostname=host,
         port=port,
-        auth=auth,
+        auth=auth,  # ty: ignore
         retain=True,
     )
     logger.info("Published '%s' to %s", payload, topic)
